0c6fbde3c43fe00ec9958240d26afa61cda4cf0b
wt-854 <wt854@outlook.com>
2024-11-06 14:37:47 +0800

    initial commit

---
+++ src/test/java/com/example/crypto_trading/CryptoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crypto_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
